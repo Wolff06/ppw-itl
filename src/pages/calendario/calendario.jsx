ae65b8dc463186b1341a100cce8faf76ead0abc8
wolff <aa.sotogarnica@hotmail.com>
2026-01-08 10:52:22 -0600

    Mejora de Front-End para páginas. Se añadió página de planes de estudio.

--- src/pages/calendario/calendario.jsx
+++ src/pages/calendario/calendario.jsx
@@ -1,7 +1,7 @@
 
 
 import Seccion from "../../assets/templates/seccion/seccion.jsx";
-import download from "/icons/download.svg?url";
+import download from "/src/assets/icons/download.svg";
 import styles from "./calendario.module.css";
 
 export default function Calendario() {
